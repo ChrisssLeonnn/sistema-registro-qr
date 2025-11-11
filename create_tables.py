a6816c8ab743e8a0188a2724d6f8fd940d8e7dfb
Christopher Leon <christopherleon@192.168.68.102>
2025-11-11 00:55:36 -0600

    Use Flask CLI to create DB tables

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-# This script is used by Render to initialize the database tables.
-from app import app, db
-
-with app.app_context():
-    print("Creating database tables...")
-    db.create_all()
-    print("Tables created successfully.")
